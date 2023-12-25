348479d2fda6fa8c8b37d5f6a01eed511d23910d
Sin3d <Sin3d@HOME-PC>
2023-12-25 15:36:18 +0300

    Добавьте файлы проекта.

---
+++ metod1/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Metod1
+{
+    static void Main()
+    {
+        Stack<int> stek = new Stack<int>();
+
+        string command;
+        while ((command = Console.ReadLine()) != "exit")
+        {
+            string[] parts = command.Split();
+            string op = parts[0];
+
+            switch (op)
+            {
+                case "push":
+                    try
+                    {
+                        int number = int.Parse(parts[1]);
+
+                        stek.Push(number);
+                        Console.WriteLine("ok");
+                        break;
+                    }
+                    catch (Exception e)
+                    {
+                        // Console.WriteLine(e.ToString());
+                        Console.WriteLine("Некорректный ввод (вы ввели не число)");
+                        break;
+                    }
+
+                case "pop":
+                    int poppedNumber = stek.Pop();
+                    Console.WriteLine(poppedNumber);
+                    break;
+
+                case "back":
+                    int topNumber = stek.Peek();
+                    Console.WriteLine(topNumber);
+                    break;
+
+                case "size":
+                    int size = stek.Count;
+                    Console.WriteLine(size);
+                    break;
+
+                case "clear":
+                    stek.Clear();
+                    Console.WriteLine("ok");
+                    break;
+                default:
+                    Console.WriteLine("Вы написали некорректную команду для стека");
+                    break;
+            }
+        }
+
+        Console.WriteLine("Bye");
+    }
+}
